c3dec5de56c802c3e40635a5fb95765b5a9cb6db
Clément Roche <rchclement@gmail.com>
2025-02-04 20:03:08 +0100

    fix build

--- plugins/contentful/src/components/auth.tsx
+++ plugins/contentful/src/components/auth.tsx
@@ -27,7 +27,6 @@ export function Auth({
     }, [])
 
     return (
-<<<<<<< HEAD
         <>
             <form onSubmit={onSubmit} className="flex flex-col gap-[15px]">
                 <img
@@ -71,48 +70,5 @@ export function Auth({
                 </div>
             </form>
         </>
-=======
-        <form onSubmit={onSubmit} className="flex flex-col gap-2.5 text-tertiary">
-
-            <img
-                src={Logo}
-                alt="Contentful Hero"
-                className="object-contain w-full mb-1 rounded-lg h-[200px] bg-contentful-orange bg-opacity-10"
-            />
-
-
-            <div className="grid grid-cols-3 items-center gap-2.5">
-                <label htmlFor="spaceId">Space ID</label>
-                <input
-                    id="spaceId"
-                    type="text"
-                    className="w-full col-span-2"
-                    placeholder="Space ID"
-                    value={contentfulConfig.space}
-                    onChange={e => setContentfulConfig(prev => ({ ...prev, space: e.target.value }))}
-                />
-            </div>
-            <div className="grid grid-cols-3 items-center gap-2.5">
-                <label htmlFor="accessToken">Access Token</label>
-                <input
-                    id="accessToken"
-                    type="text"
-                    className="w-full col-span-2"
-                    placeholder="Access Token"
-                    value={contentfulConfig.accessToken}
-                    onChange={e => setContentfulConfig(prev => ({ ...prev, accessToken: e.target.value }))}
-                />
-            </div>
-            <div className="sticky left-0 bottom-0 flex justify-between bg-primary mt-1 items-center max-w-full">
-                <button
-                    type="submit"
-                    disabled={isLoading}
-                    className="flex justify-center items-center relative py-2 framer-button-secondary w-full"
-                >
-                    {isLoading ? "Connecting..." : "Connect"}
-                </button>
-            </div>
-        </form>
->>>>>>> 3f6b406 (css)
     )
 }
